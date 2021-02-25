4ee25293ed6c184740fd6e6e6cfb1c967b04998c
Greg1400 <herzig.gregoire@gmail.com>
2021-02-25 14:42:53 +0100

    Delete ".css" and create class "StyleApp.py"

---
+++ MFRC522-python/StyleApp.py
@@ -0,0 +1,11 @@
+# import all necessary ressources
+
+import sys, time, AdminWindow, AdminWindow, ProgressBar, Read, Write
+from PySide2 import QtGui
+from PySide2.QtWidgets import *
+from PySde2.QtGui import *
+
+#Create the CSS class
+class StyleApp():
+
+    
