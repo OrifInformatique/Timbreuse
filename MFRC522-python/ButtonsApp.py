bc37d5a50cb01d4f76043e5e555a67bbd10d6d77
Greg1400 <herzig.gregoire@gmail.com>
2021-03-02 11:42:21 +0100

    modifiy adminlogin.window() to adminlogin.mainwindow()

--- MFRC522-python/ButtonsApp.py
+++ MFRC522-python/ButtonsApp.py
@@ -90,7 +90,7 @@ class MainWindow():
     def adminApp(self):
         adminInfo = QMessageBox.question(self.window, "Admin", "Voulez-vous vous connecter au compte administrateur ?", QMessageBox.Yes | QMessageBox.No)
         if adminInfo == QMessageBox.Yes:
-            self.windowAdminLogin = AdminLogin.Window()
+            self.windowAdminLogin = AdminLogin.MainWindow()
             self.windowAdminLogin.showFullScreen()
         elif adminInfo == QMessageBox.No:
             pass
