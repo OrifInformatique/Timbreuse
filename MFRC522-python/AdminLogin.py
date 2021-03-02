8453141f52cd31063635d4fb6151573daab07553
Greg1400 <herzig.gregoire@gmail.com>
2021-03-02 13:58:10 +0100

    comment TEST PyQT, uncomment userLine...

--- MFRC522-python/AdminLogin.py
+++ MFRC522-python/AdminLogin.py
@@ -75,47 +75,47 @@ class MainWindow():
 #                                       TEST OF PYQT.QTextEdit
 #******************************************************************************************************
 
-    # Create user text edit
-    def setUserText(self):
-        self.pseudo = QtWidgets.QTextEdit(self.window)
-        self.pseudo.setGeometry(60,150,250,40)
-        pseudo = self.pseudo.setText()
-        return pseudo == "Admin"
+    # # Create user text edit
+    # def setUserText(self):
+    #     self.pseudo = QtWidgets.QTextEdit(self.window)
+    #     self.pseudo.setGeometry(60,150,250,40)
+    #     pseudo = self.pseudo.setText()
+    #     return pseudo == "Admin"
     
-    # Create password text edit
-    def setPasswordText(self):
-        self.password = QtWidgets.QTextEdit(self.window)
-        self.password.setGeometry(60,300,250,40)
-        password = self.password.setText()
-        return password == "Password"
+    # # Create password text edit
+    # def setPasswordText(self):
+    #     self.password = QtWidgets.QTextEdit(self.window)
+    #     self.password.setGeometry(60,300,250,40)
+    #     password = self.password.setText()
+    #     return password == "Password"
 
-    # VALIDATE
-    def validate(self):
-        if(self.pseudo() and self.password()):
-            self.windowAdminWindow == AdminWindow.MainWindow()
-            self.windowAdminWindow.showFullScreen()
-        else:
-            QMessageBox.information(self.window, "Erreur", "Vous n'avez pas le droit à l'accès.")
+    # # VALIDATE
+    # def validate(self):
+    #     if(self.pseudo() and self.password()):
+    #         self.windowAdminWindow == AdminWindow.MainWindow()
+    #         self.windowAdminWindow.showFullScreen()
+    #     else:
+    #         QMessageBox.information(self.window, "Erreur", "Vous n'avez pas le droit à l'accès.")
 
 #******************************************************************************************************
 #                                       END TEST OF PYQT.QTextEdit
 #******************************************************************************************************
     #Create the text's line
-#     def setUserLine(self):
-#         user = self.userLine.text()
-#         return user == "Admin"
-# 
-#     def setPasswordLine(self):
-#         password = self.passwordLine.text()
-#         return password == "Password"
-# 
-#     #Validate the login
-#     def validation(self):
-#         if (self.setUserLine() and self.setPasswordLine()) : 
-#             self.windowAdminWindow = AdminWindow.Window()
-#             self.windowAdminWindow.showFullScreen()
-#         
-#         else :
-#             QMessageBox.information(self.window, "Erreur", "Vous n'avez pas le droit à l'accès.")
+     def setUserLine(self):
+         user = self.userLine.text()
+         return user == "Admin"
+ 
+     def setPasswordLine(self):
+         password = self.passwordLine.text()
+         return password == "Password"
+ 
+     #Validate the login
+     def validation(self):
+         if (self.setUserLine() and self.setPasswordLine()) : 
+             self.windowAdminWindow = AdminWindow.Window()
+             self.windowAdminWindow.showFullScreen()
+         
+         else :
+             QMessageBox.information(self.window, "Erreur", "Vous n'avez pas le droit à l'accès.")
 
 main = MainWindow()
