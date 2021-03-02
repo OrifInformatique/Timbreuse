815ba9ca2f33dd467101e05957cb0a10769495c4
Greg1400 <herzig.gregoire@gmail.com>
2021-03-02 13:09:56 +0100

    Test of QTextEdit instead of userLine and PasswordLine

--- MFRC522-python/ButtonsApp.py
+++ MFRC522-python/ButtonsApp.py
@@ -100,4 +100,4 @@ class MainWindow():
         self.windowProgressBar = ProgressBar.Window()
         self.windowProgressBar.show()
 # Instantiate object window
-main = ButtonsApp.MainWindow()
+main = MainWindow()
