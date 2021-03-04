72ec1442129f0b0a3c39e98e31b19138f21f5adf
Greg1400 <herzig.gregoire@gmail.com>
2021-03-04 09:43:00 +0100

    Making main window class with methods in it for different windows

---
+++ MFRC522-python/Timbreuse.py
@@ -0,0 +1,66 @@
+# Necessary imports
+import sys
+# import PySide2 as p2
+# from p2 import QtCore, QtWidgets, QtGui
+# from p2.QtWidgets import *
+# from p2.QtGui import *
+# from p2.QtCore import *
+
+class MainWindow():
+    # Declare boolean in order to show what's necessary
+    base = True
+    admin = False
+    config = False
+
+    def __init__(self):
+        super().__init__()
+
+        #Creation of the app
+        app = QtWidgets.QApplication(sys.argv)
+
+        # Create window
+        self.window = QtWidgets.QMainWindow()
+
+        # Set window title
+        self.window.setWindowTitle("Timbreuse")
+
+        # call the init gui
+        self.initGui()
+
+        # set file css as stylesheet
+        app.setStyleSheet(open('./style.css').read())
+
+        # Set window in full screen
+        self.window.showFullScreen()
+
+    def initGui(self):
+
+        #set Icon
+        self.setIcon()
+
+        if(base == True):
+            print("base")
+            # self.baseWindow()
+        
+        elif(admin == True):
+            print("admin")
+            # self.adminWindow()
+        
+        elif(config == True):
+            print("config")
+            # self.configWindow()
+        
+    
+    # Insert Icon
+    def setIcon(self):
+        self.window.setIcon(QtGui.QIcon('logo_orif_square_transparent.png'))
+    
+    # Window with in/out button
+    def baseWindow(self):
+        pass
+    # Window for admin
+    def AdminWindow(self):
+        pass
+    # Window to link RFID card to user
+    def ConfigWindow(self):
+        pass
