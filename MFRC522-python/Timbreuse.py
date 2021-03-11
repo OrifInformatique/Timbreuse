fd8043dc29eeec8e2a0f63c152d69e2f144f621d
Greg1400 <herzig.gregoire@gmail.com>
2021-03-11 15:09:32 +0100

    try resolve label UID and all Name text not showing

--- MFRC522-python/Timbreuse.py
+++ MFRC522-python/Timbreuse.py
@@ -269,11 +269,11 @@ class MainWindow():
         self.labelName.setText("Name User")
         self.labelName.setReadOnly(True)
         
-        self.labelName.setFixedSize(100,50)
-        self.labelName.move(50,250)
+        # self.labelName.setFixedSize(100,50)
+        # self.labelName.move(50,250)
         
-        self.lineName.setFixedSize(300,50)
-        self.lineName.move(175,250)
+        # self.lineName.setFixedSize(300,50)
+        # self.lineName.move(175,250)
 
         # set name for label
         self.labelUID.setObjectName("labelUID")
@@ -281,11 +281,12 @@ class MainWindow():
         # set stylesheet for label
         self.labelUID.setStyleSheet(open('./style.css').read())
         self.labelName.setStyleSheet(open('./style.css').read())
-
-        self.lineUID.show()
+        
         self.labelUID.show()
-        self.lineName.show()
+        self.lineUID.show()
         self.labelName.show()
+        self.lineName.show()
+        
         self.setButtonBackToConfigure()
 
     def setButtonBackToConfigure(self):
