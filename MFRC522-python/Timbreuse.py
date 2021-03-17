099a9368661cdf634d607e2cef3837bada794aba
Greg1400 <herzig.gregoire@gmail.com>
2021-03-17 13:59:31 +0100

    create independant ProgressingBar file

--- MFRC522-python/Timbreuse.py
+++ MFRC522-python/Timbreuse.py
@@ -9,7 +9,9 @@ from PySide2.QtCore import *
 # import from python files in MFRC522
 import Read
 import Write
-# import ProgressBar
+import ProgressingBar
+from ProgressingBar import *
+
 
 
 class MainWindow():
@@ -162,7 +164,7 @@ class MainWindow():
         QMessageBox.information(self.window, "Badge", "Vous avez 10 secondes pour passer le bagde.")
         
         
-        self.windowProgressBar = ProgressBar()
+        self.windowProgressBar = ProgressingBar()
         # self.windowProgressBar.show()
     
     '''
@@ -252,7 +254,6 @@ class MainWindow():
     # define write app
     def WriteApp(self):
         Write.Write()
-        self.progressBar()
         
     '''
                                 WINDOW CONFIGURATION
@@ -418,43 +419,5 @@ class MainWindow():
         
         exec(f"self.buttonBackConf.clicked.connect(self.{last_page})")
         self.buttonBackConf.show()
-        
-        
-class ProgressBar(MainWindow):
-    
-    def __init__(self):
-        super().__init__()
-        self.Read = Read.Read()
-        
-        self.progressBar = QProgressBar()
-
-
-    def ProgressBar(self):
-        self.progressBar.setGeometry(30,40,200,25)
-        
-        # Set timer
-        self.timer = QBasicTimer()
-        
-        # Set step (=initial position)
-        self.step = self.Read.startTime
-        
-        
-        self.progressBar.show(self.window)
-        
-        
-    def startProgress(self):
-        if self.timer.isActive():
-            self.timer.stop()
-        else:
-            self.timer.start(100,self)
-
-    def timerEvent(self, event):
-        if self.step >= 100:
-            self.timer.stop()
-            self.setButtonBackToConfigure("baseWindow")
-            return
-        self.step += 1
-        self.progressBar.setValue(self.step)
-
 
 main = MainWindow()
