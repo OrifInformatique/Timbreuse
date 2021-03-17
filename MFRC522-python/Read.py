224faf28765adc49d0e04aa6badd479cc7d4e6d4
Greg1400 <herzig.gregoire@gmail.com>
2021-03-17 15:45:31 +0100

    scan uid and return it

--- MFRC522-python/Read.py
+++ MFRC522-python/Read.py
@@ -86,7 +86,8 @@ class Read():
     def uid(self):
         MIFAREReader = MFRC522.MFRC522()
         (status,uid) = MIFAREReader.MFRC522_Anticoll()
-        return uid
+        self.returnUID = "%s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
+        return self.returnUID
             
                     
 
