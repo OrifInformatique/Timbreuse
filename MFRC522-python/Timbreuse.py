515347e74cd39da5738c42c1126a168385a5d0df
Greg1400 <herzig.gregoire@gmail.com>
2021-03-18 07:43:20 +0100

    scan uid and return it

--- MFRC522-python/Timbreuse.py
+++ MFRC522-python/Timbreuse.py
@@ -416,11 +416,11 @@ class MainWindow():
         
         self.setButtonBackToConfigure("configWindow")
     def scanningRFID(self):
-        self.read = Read.Read()
+        read = Read()
         # Create an object of the class MFRC522
         MIFAREReader = MFRC522.MFRC522()
         (status,uid) = MIFAREReader.MFRC522_Anticoll()
-        self.uid = self.read.uid()
+        self.uid = read.uid()
         print("SCANUID : " +self.uid)
         
     def setButtonBackToConfigure(self, last_page:str):
