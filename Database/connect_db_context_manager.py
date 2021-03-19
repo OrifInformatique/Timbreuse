c86a534b508d0bc798c9ece5185cb0aaef0406fd
Greg1400 <herzig.gregoire@gmail.com>
2021-03-19 14:32:15 +0100

    test interaction BDD

--- Database/connect_db_context_manager.py
+++ Database/connect_db_context_manager.py
@@ -13,14 +13,24 @@ connection = MC.connect(host=host,
                        user=user,
                        password=password,
                        database=db)
+
 cursor = connection.cursor()
+# cursor.execute("DESCRIBE t_timbreuse")
+# cursor.execute("INSERT INTO t_timbreuse (nom_timbreuse) VALUES ('SIBEC')")
+# cursor.execute("INSERT INTO t_assures (nom_assures,prenom_assures,username_assures) VALUES (%s,%s,%s)", ("HERZIG","Grégoire","hegr"))
+# connection.commit()
 
+# for x in cursor:
+    # print(x)
+# cursor.execute("SELECT * FROM t_assures")
 
-print(cursor.execute("SELECT * FROM t_timbreuse"))
-
+# for x in cursor:
+#     print(x)
+    
 if(connection.is_connected()):
     print("IN DB")
-    print(connection)
+    # print(connection)
+    # print(cursor.execute("SELECT * FROM t_timbreuse"))    
     cursor.close()
     connection.close()
         
