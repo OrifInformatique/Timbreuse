6e7c8252eaa0166bfbce254518dca132af443c3a
Greg1400 <herzig.gregoire@gmail.com>
2021-03-19 11:23:28 +0100

    Modif connect

--- Database/connect_db_context_manager.py
+++ Database/connect_db_context_manager.py
@@ -2,29 +2,26 @@
 
 import mysql.connector as MC
 
-class DataBase():
-    def __init__(self):
-        self.host = '127.0.0.1'
-        self.user = 'admin'
-        self.password = 'OrifInfo2009'
-        self.db = "timbreuse-orif"
+
+host = '127.0.0.1'
+user = 'admin'
+password = 'OrifInfo2009'
+db = "timbreuse-orif"
+    
+
+connection = MC.connect(host=host,
+                       user=user,
+                       password=password,
+                       database=db)
+cursor = connection.cursor()
+
+
+print(cursor.execute("SELECT * FROM t_timbreuse"))
+
+if(connection.is_connected()):
+    print("IN DB")
+    print(connection)
+    cursor.close()
+    connection.close()
         
-        
-        try:
-            #  Connection to BD
-            self.connection = MC.connect(host=self.host,
-                                   user=self.user,
-                                   password=self.password,
-                                   database=self.db)
-            self.cursor = self.connection.cursor()
-            print("IN DB")
-            
-        except (Exception,
-                ConnectionRefusedError,
-                pymysql.err.OperationalError,
-                pymysql.err.DatabaseError) as err:
-            print(err)
-        finally:
-            if(self.connection.is_connected()):
-                self.cursor.close()
-                self.connection.close()
+
