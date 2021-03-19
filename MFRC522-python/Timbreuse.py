07473c0ca5a64fac2d5a13e9abe3e56f462beec2
Greg1400 <herzig.gregoire@gmail.com>
2021-03-19 14:52:09 +0100

    testing connection to db through app

--- MFRC522-python/Timbreuse.py
+++ MFRC522-python/Timbreuse.py
@@ -8,6 +8,7 @@ from PySide2 import QtCore, QtWidgets, QtGui
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from PySide2.QtCore import *
+import mysql.connector as MC
 
 # import from python files in MFRC522
 import Read
@@ -25,7 +26,24 @@ class MainWindow():
     config = False
 
     
+    def DataBaseConnection(self):
+        try:
+            host = '127.0.0.1'
+            user = 'admin'
+            password = 'OrifInfo2009'
+            db = "timbreuse-orif"
+    
 
+            connection = MC.connect(host=host,
+                                    user=user,
+                                    password=password,
+                                    database=db)
+            QMessageBox.about(self, "Connection", "Connected to DataBase")
+        except MC.Error as err:
+            QMessageBox.warning(self, "Failed", "Failed to connect to database")
+            
+            sys.exit(app.exec_())
+            
     def __init__(self):
         super().__init__()
         #Creation of the app
@@ -40,6 +58,8 @@ class MainWindow():
         
         # define all objects
             # Button
+            # Database connection testing
+        self.db = QPushButton(self.window)
         self.buttonAdmin = QPushButton(self.window)
         self.buttonAdmin.hide()
         
@@ -142,7 +162,15 @@ class MainWindow():
         self.setButtonIn()
         self.setButtonOut()
         self.setButtonAdmin()
+        self.databaseButton()
         
+    def databaseButton(self):
+       self.db.setText("db") 
+       self.db.setFixedSize(50,50)
+       self.db.move(90,10)
+       self.db.clicked.connect(self.DataBaseConnection)
+       self.db.show()
+       
      # Create Button IN
     def setButtonIn(self):
         
