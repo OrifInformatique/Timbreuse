39f0dd3d642a867f060aad90994dbbea1691b84f
Greg1400 <herzig.gregoire@gmail.com>
2021-03-23 11:46:17 +0100

    Affichage section list

--- MFRC522-python/Timbreuse.py
+++ MFRC522-python/Timbreuse.py
@@ -365,7 +365,7 @@ class MainWindow():
             # print(records)
             print_records = ''
             for record in records:
-                print_records += str(record[1])
+                print_records += str(record[0])
                 self.scrollableListSection.addItem(print_records)
                 
         except MC.Error as err:
